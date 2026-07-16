9fe3fef2646df153ed254992cf629f589fb88057
root <root@localhost.localdomain>
2026-07-16 14:20:04 +0400

    Connect Create Project button

--- lib/features/projects/screens/new_project_screen.dart
+++ lib/features/projects/screens/new_project_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../services/project_service.dart';
 
 class NewProjectScreen extends StatefulWidget {
   const NewProjectScreen({super.key});
@@ -14,9 +15,7 @@ class _NewProjectScreenState extends State<NewProjectScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("New Project"),
-      ),
+      appBar: AppBar(title: const Text("New Project")),
       body: ListView(
         padding: const EdgeInsets.all(16),
         children: [
@@ -35,25 +34,43 @@ class _NewProjectScreenState extends State<NewProjectScreen> {
               border: OutlineInputBorder(),
             ),
           ),
-          const SizedBox(height: 16),
+          const SizedBox(height: 20),
+
           const ListTile(
             leading: Icon(Icons.android),
             title: Text("Language"),
             subtitle: Text("Kotlin"),
           ),
+
           const ListTile(
             leading: Icon(Icons.settings),
             title: Text("Minimum SDK"),
             subtitle: Text("Android 7.0 (API 24)"),
           ),
+
           const ListTile(
             leading: Icon(Icons.settings_applications),
             title: Text("Target SDK"),
             subtitle: Text("Android 16 (API 36)"),
           ),
+
           const SizedBox(height: 24),
+
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () async {
+              await ProjectService.createProject(
+                projectName: projectController.text.trim(),
+                packageName: packageController.text.trim(),
+              );
+
+              if (!context.mounted) return;
+
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text("Project Created Successfully"),
+                ),
+              );
+            },
             child: const Text("Create Project"),
           ),
         ],
