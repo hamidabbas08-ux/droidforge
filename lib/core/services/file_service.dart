3b28750f5ab4fcc5d081272f966a8ce20df0227c
root <root@localhost.localdomain>
2026-07-16 21:58:03 +0400

    Use path_provider for file storage

--- lib/core/services/file_service.dart
+++ lib/core/services/file_service.dart
@@ -1,29 +1,41 @@
 import 'dart:io';
 
+import 'package:path_provider/path_provider.dart';
+
 class FileService {
-  static final Directory _projectDir =
-      Directory("${Directory.current.path}/myapp");
+  static Future<Directory> _projectDir() async {
+    final docs = await getApplicationDocumentsDirectory();
+    final dir = Directory('${docs.path}/myapp');
+
+    if (!await dir.exists()) {
+      await dir.create(recursive: true);
+    }
+
+    return dir;
+  }
 
   static Future<String> readFile(String relativePath) async {
     try {
-      final file = File("${_projectDir.path}/$relativePath");
+      final projectDir = await _projectDir();
+      final file = File('${projectDir.path}/$relativePath');
 
       if (!await file.exists()) {
         await file.create(recursive: true);
-        await file.writeAsString("");
+        await file.writeAsString('');
       }
 
       return await file.readAsString();
     } catch (e) {
-      return "// Error reading file\n$e";
+      return '// Error reading file\n$e';
     }
   }
 
   static Future<void> saveFile(
-      String relativePath,
-      String content,
+    String relativePath,
+    String content,
   ) async {
-    final file = File("${_projectDir.path}/$relativePath");
+    final projectDir = await _projectDir();
+    final file = File('${projectDir.path}/$relativePath');
 
     if (!await file.exists()) {
       await file.create(recursive: true);
