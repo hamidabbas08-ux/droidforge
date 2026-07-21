e52515e83077beace00307e94888bcd6fc23d5a6
root <root@localhost.localdomain>
2026-07-21 14:20:31 +0400

    Use permanent signing key for updateable debug APKs

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,19 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    // The Flutter Gradle Plugin must be applied after Android and Kotlin plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+
+if (keystorePropertiesFile.exists()) {
+    FileInputStream(keystorePropertiesFile).use {
+        keystoreProperties.load(it)
+    }
 }
 
 android {
@@ -24,31 +36,56 @@ android {
         ndk {
             abiFilters += listOf("arm64-v8a")
         }
-        
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+
         applicationId = "com.hamid.droidforge"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
-    
+    signingConfigs {
+        if (keystorePropertiesFile.exists()) {
+            create("droidforgePermanent") {
+                val configuredStoreFile =
+                    keystoreProperties.getProperty("storeFile")
+                        ?: error("storeFile is missing from key.properties")
+
+                storeFile = file(configuredStoreFile)
+                storePassword =
+                    keystoreProperties.getProperty("storePassword")
+                        ?: error("storePassword is missing from key.properties")
+                keyAlias =
+                    keystoreProperties.getProperty("keyAlias")
+                        ?: error("keyAlias is missing from key.properties")
+                keyPassword =
+                    keystoreProperties.getProperty("keyPassword")
+                        ?: error("keyPassword is missing from key.properties")
+            }
+        }
+    }
 
     buildTypes {
+        debug {
+            if (keystorePropertiesFile.exists()) {
+                signingConfig = signingConfigs.getByName("droidforgePermanent")
+            }
+        }
+
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            if (keystorePropertiesFile.exists()) {
+                signingConfig = signingConfigs.getByName("droidforgePermanent")
+            } else {
+                signingConfig = signingConfigs.getByName("debug")
+            }
         }
     }
 }
 
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        jvmTarget =
+            org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
 }
 
